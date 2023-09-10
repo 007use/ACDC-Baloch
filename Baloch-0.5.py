56fa3429d61f85a34cc52ae74b9451e5fbd743e1
Ko Shar-ACDC-404 <105577948+007use@users.noreply.github.com>
2023-09-10 19:37:59 +0630

    Add files via upload

---
+++ Baloch-0.5.py
@@ -0,0 +1,11 @@
+import os,platform
+os.system('git pull')
+print('\033[1;32mMr BALOXH') 
+print(" \033[1;31mJoin Whatsapp Group")
+os.system('xdg-open https://chat.whatsapp.com/CGM6VPP6ILAHpp88PTiZ79')
+ 
+trt=platform.architecture()[0]
+if trt=="32bit":
+    __import__("b1")
+elif trt=="64bit":
+    __import__("b1")
